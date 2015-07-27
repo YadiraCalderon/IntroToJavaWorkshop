6b5c48f372c3b0bbcf6a0ad07e3ca40778c72659
league-workshopper-6 <workshop@Mac-mini.local>
2015-07-27 14:53:51 -0700

    Robot Drawing Shapes

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,14 +1,29 @@
 package day1.robot;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
+	private static Color red;
+
 	public static void main(String[] args) {
 		
 		// START HERE
-
-		new Robot();
+Robot yadira= new Robot ();
+	for (int i = 0; i < 4; i++) {
 		
+		yadira.setPenColor(Color.MAGENTA);
+		yadira.setPenWidth(10);
+		yadira.setSpeed(10);
+		yadira.penDown();
+		yadira.move(50);
+		yadira.turn(180);
 		
 	}
-}
+		
+	}
+
+}	
+
+
